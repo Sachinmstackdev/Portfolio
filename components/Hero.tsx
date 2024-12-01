6d1d8c617b67d589aeab9d74dfa794b9a827001c
sachin kumar <109363151+Sachinmstackdev@users.noreply.github.com>
2024-12-01 09:27:29 +0530

    indexing the website

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -42,10 +42,10 @@ const Hero = () => {
     <div className="flex justify-center relative my-20 z-10">
       <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
         <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-          Digital Marketing & SEO Expert
+          Digital Marketer & SEO Expert
         </h2>
         <TextGenerateEffect
-          words="Driving Business Growth Through SEO & Digital Solutions"
+          words="Driving Business Growth Through Digital Marketing"
           className="text-center text-[40px] md:text-5xl lg:text-6xl"
         />
 
